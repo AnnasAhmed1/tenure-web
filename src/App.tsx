0b9bb437cb71f3de1a2a22fdceb4f16bab25db3d
eashaasif123 <annasahmed1609@gmail.com>
2023-10-15 15:36:46 +0500

    routing check1

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 // import Layout from './components/Layout/Layout';
 // import { SplashScreen } from './pages/IntroScreens/SpashScreen';
-import SignUp from './pages/SignUp/SignUp.tsx';
+// import SignUp from './pages/SignUp/SignUp.tsx';
 import Login from './pages/Login/Login.tsx';
-import Forgetpassword from './pages/ForgetPassword/ForgetPassword.tsx';
-import ResetPassword from './pages/ForgetPassword/ResetPassword.tsx';
-import Sucess from './pages/ForgetPassword/Sucess.tsx';
-import Email_sent from './pages/ForgetPassword/Email_sent.tsx';
-import Step1 from './pages/OnBoarding/Step1.tsx';
-import Step2 from './pages/OnBoarding/Step2.tsx';
-import Step3 from './pages/OnBoarding/Step3.tsx';
-import Step4_dc from './pages/OnBoarding/Step4_dc.tsx';
-import Layout from './components/Layout/Layout.tsx';
+// import Forgetpassword from './pages/ForgetPassword/ForgetPassword.tsx';
+// import ResetPassword from './pages/ForgetPassword/ResetPassword.tsx';
+// import Sucess from './pages/ForgetPassword/Sucess.tsx';
+// import Email_sent from './pages/ForgetPassword/Email_sent.tsx';
+// import Step1 from './pages/OnBoarding/Step1.tsx';
+// import Step2 from './pages/OnBoarding/Step2.tsx';
+// import Step3 from './pages/OnBoarding/Step3.tsx';
+// import Step4_dc from './pages/OnBoarding/Step4_dc.tsx';
+// import Layout from './components/Layout/Layout.tsx';
 
 export default function App() {
   return <Login />;
